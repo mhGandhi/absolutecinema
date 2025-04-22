146634439d2542b8823e9f89863012ace6c463f0
Adrian <adrian.akipi@web.de>
2025-04-23 01:45:40 +0200

    draw stuff

--- src/main/java/net/absolutecinema/AbsoluteCinema.java
+++ src/main/java/net/absolutecinema/AbsoluteCinema.java
@@ -1,14 +1,21 @@
 package net.absolutecinema;
 
+import net.absolutecinema.rendering.Camera;
 import net.absolutecinema.rendering.GraphicsWrapper;
 import net.absolutecinema.rendering.Window;
+import net.absolutecinema.rendering.meshes.BufferWrapper;
 import net.absolutecinema.rendering.shader.Shader;
 import net.absolutecinema.rendering.shader.ShaderProgram;
 import net.absolutecinema.rendering.shader.ShaderType;
+import net.absolutecinema.rendering.shader.Uni;
+import org.joml.Matrix4f;
 import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.system.MemoryUtil;
 
+import java.nio.FloatBuffer;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Map;
 
 import static net.absolutecinema.rendering.GraphicsWrapper.initGLFW;
 import static org.lwjgl.glfw.GLFW.*;
@@ -22,6 +29,13 @@ public class AbsoluteCinema {
     private Window window;
 
     private final GameConfig config;
+
+    Uni<Matrix4f> view;
+    Uni<Matrix4f> projection;
+    Uni<Matrix4f> model;
+    BufferWrapper cube;
+
+    Camera cam;
 
     public AbsoluteCinema(final GameConfig pConfig){
         instance = this;
@@ -62,31 +76,55 @@ public class AbsoluteCinema {
         glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 
         {
-            ShaderProgram sp = new ShaderProgram();
+            ShaderProgram shaderProgram = new ShaderProgram();
             Path shaderpath = config.assetDirectory().toPath().resolve("shader");
             try{
                 Shader vsh = new Shader(ShaderType.VERTEX, Files.readString(shaderpath.resolve("shader.vert")));
                 Shader fsh = new Shader(ShaderType.FRAGMENT, Files.readString(shaderpath.resolve("shader.frag")));
-                sp.attach(vsh);
-                sp.attach(fsh);
+                shaderProgram.attach(vsh);
+                shaderProgram.attach(fsh);
             }catch (Exception e){
                 e.printStackTrace();
             }
-            sp.link();
-            sp.use();
+            shaderProgram.link();
+            shaderProgram.use();
+
+            view = new Uni<>(shaderProgram, "view");
+            projection = new Uni<>(shaderProgram, "projection");
+            model = new Uni<>(shaderProgram, "model");
+
+            cam = new Camera();
+            //cam.setX(10);
+
+            view.set(cam.getViewMatrix());
+            projection.set(cam.getProjectionMatrix((float) Math.toRadians(90.0f), ((float) 800 / (float) 600), 0.1f, 100.0f));
+            model.set(new Matrix4f().identity());
         }
+
+        cube = new BufferWrapper(6);
+        float[] cubeVts = Util.trisFromObj(config.assetDirectory().toPath().resolve("cube.obj"));
+        FloatBuffer vertexBuffer = MemoryUtil.memAllocFloat(cubeVts.length);
+        vertexBuffer.put(cubeVts).flip();
+        cube.uploadToVBO(vertexBuffer);
+        cube.addVToV(3,false,0);
+        cube.addVToV(3,true,3);
     }
 
     private long frameCount = 0;
     private void frame(){
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         frameCount++;
         glfwPollEvents();
         if(window.shouldClose()){
             running = false;
         }
 
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        view.set(cam.getViewMatrix());
+        cube.bindVAO();
+        cube.draw();
 
+
+        window.swapBuffers();
         if(frameCount%100_000==0) System.out.println(frameCount);
     }
 }
--- src/main/java/net/absolutecinema/rendering/meshes/BufferWrapper.java
+++ src/main/java/net/absolutecinema/rendering/meshes/BufferWrapper.java
@@ -8,24 +8,31 @@ public class BufferWrapper {
     private Vao vao;
     private Vbo vbo;
     private int lastIndex;
+    private int stride;
+    private int vertCount;
 
-    public BufferWrapper(){
+    public BufferWrapper(int pStride){
         vao = new Vao();
         vbo = new Vbo();
         lastIndex = -1;
+        vertCount = 0;
+
+        this.stride = pStride;
     }
 
     public void uploadToVBO(FloatBuffer pBuffer){
         vbo.bind();
         GraphicsWrapper.uploadToVBO(pBuffer);
+
+        vertCount = pBuffer.limit() / stride;
         //MemoryUtil.memFree(pBuffer);
     }
 
-    public void addVToV(int pSize, boolean pNormalize, int pStride, int pOffset){
+    public void addVToV(int pSize, boolean pNormalize, int pOffset){
         vao.bind();
         vbo.bind();
         lastIndex++;
-        GraphicsWrapper.assignVToV(lastIndex, pSize, pNormalize, pStride, pOffset);
+        GraphicsWrapper.assignVToV(lastIndex, pSize, pNormalize, this.stride, pOffset);
         GraphicsWrapper.enableVToV(lastIndex);
     }
 
@@ -36,6 +43,10 @@ public class BufferWrapper {
 
     public void bindVAO(){
         this.vao.bind();
+    }
+
+    public void draw(){
+        GraphicsWrapper.drawTriangles(vertCount);
     }
 
 }
