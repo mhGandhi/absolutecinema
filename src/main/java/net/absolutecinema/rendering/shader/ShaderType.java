bb83b0c161edb4644b01ec9c9e6522d1228bc01b
Adrian <adrian.akipi@web.de>
2025-05-04 13:47:58 +0200

    file extension integrated into Type enum

--- src/main/java/net/absolutecinema/rendering/shader/ShaderType.java
+++ src/main/java/net/absolutecinema/rendering/shader/ShaderType.java
@@ -1,6 +1,14 @@
 package net.absolutecinema.rendering.shader;
 
 public enum ShaderType {
-    VERTEX,
-    FRAGMENT
+    VERTEX("vertex", ".vert"),
+    FRAGMENT("fragment", ".frag");
+
+    public final String name;
+    public final String extension;
+
+    private ShaderType(final String pName, final String pExtension) {
+        this.name = pName;
+        this.extension = pExtension;
+    }
 }
