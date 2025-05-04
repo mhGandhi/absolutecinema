ac67f0bb8b26b041142d11af875f22d71c147335
Adrian <adrian.akipi@web.de>
2025-05-04 15:42:25 +0200

    added exception when SP doesnt support uniform

--- src/main/java/net/absolutecinema/rendering/shader/Uni.java
+++ src/main/java/net/absolutecinema/rendering/shader/Uni.java
@@ -6,6 +6,9 @@ import net.absolutecinema.rendering.GraphicsWrapper;
 public class Uni<T> extends GLObject {
     public Uni(ShaderProgram pProgram, CharSequence pName){
         super(GraphicsWrapper.getUniformLocation(pProgram.id, pName));
+        if(this.id==0){
+            throw new UniformException("ShaderProgram ["+pProgram+"] does not support uniform ["+pName+"]");
+        }
     }
 
     public void set(T pVal){
---
+++ src/main/java/net/absolutecinema/rendering/shader/UniformException.java
@@ -0,0 +1,7 @@
+package net.absolutecinema.rendering.shader;
+
+public class UniformException extends RuntimeException {
+    public UniformException(String message) {
+        super(message);
+    }
+}
