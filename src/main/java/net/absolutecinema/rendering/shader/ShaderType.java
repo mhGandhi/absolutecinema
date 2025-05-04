9fef15926f03045b5cb8cc805fa410f4f846a022
Adrian <adrian.akipi@web.de>
2025-05-04 15:04:48 +0200

    logging

--- src/main/java/net/absolutecinema/rendering/shader/ShaderType.java
+++ src/main/java/net/absolutecinema/rendering/shader/ShaderType.java
@@ -1,5 +1,10 @@
 package net.absolutecinema.rendering.shader;
 
+import java.io.File;
+import java.nio.file.Path;
+
+import static net.absolutecinema.AbsoluteCinema.LOGGER;
+
 public enum ShaderType {
     VERTEX("vertex", ".vert"),
     FRAGMENT("fragment", ".frag");
@@ -10,5 +15,16 @@ public enum ShaderType {
     private ShaderType(final String pName, final String pExtension) {
         this.name = pName;
         this.extension = pExtension;
+    }
+
+    public static ShaderType byPath(Path pPath) {
+        for (ShaderType shaderType : values()) {
+            if (pPath.toString().endsWith(shaderType.extension)) {
+                LOGGER.info("Shader at ["+pPath.getFileName()+"] is of type "+shaderType.name);
+                return shaderType;
+            }
+        }
+
+        return null;
     }
 }
